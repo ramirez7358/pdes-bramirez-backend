fa0d11a7fda929d17fa49294783ef587040ad282
Brian Ramirez <ramirez7358@gmail.com>
2024-07-09 01:11:11 -0300

    Hotfix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -49,7 +49,7 @@ export class AuthService {
 
       const user = await this.userRepository.findOne({
         where: { email },
-        select: { email: true, password: true, id: true },
+        select: { email: true, password: true, id: true, fullName: true },
       });
 
       if (!user)

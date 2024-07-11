30f16b38086ad740bea8fc82a1c1ad7288ea103b
Brian Ramirez <ramirez7358@gmail.com>
2024-07-11 15:25:43 -0300

    Some fixes

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -26,10 +26,10 @@ export class CreateUserDTO {
   @IsString()
   @MinLength(6)
   @MaxLength(50)
-  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  /*@Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'The password must have a Uppercase, lowercase letter and a number',
-  })
+  })*/
   readonly password: string;
 
   @ApiProperty({
